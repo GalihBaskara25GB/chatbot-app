3c54852bcd09b079a264aa3a480d59ad12ebce54
Galih Baskara <galihbaskara25.gb@gmail.com>
2025-01-15 17:55:18 +0700

    ui improvement

--- components/BrandPopover.tsx
+++ components/BrandPopover.tsx
@@ -9,14 +9,14 @@ export function BrandPopover() {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant="outline" className="bg-transparent hover:bg-white border-white text-white hover:text-neutral-900">ChatGPT 4o mini</Button>
+        <Button variant="outline" className="bg-transparent hover:bg-white border-white text-white hover:text-neutral-900">GPT 4o mini</Button>
       </PopoverTrigger>
       <PopoverContent className="w-80 bg-neutral-400 border-neutral-400">
         <div className="grid gap-4">
           <div className="space-y-2">
-            <h4 className="font-medium leading-none">Powered by <span className="bg-cyan-400 text-white p-1 rounded font-semibold">ChatGPT 4o mini</span></h4>
+            <h4 className="font-medium leading-none">Powered by <span className="bg-cyan-400 text-white p-1 rounded font-semibold">GPT 4o mini</span></h4>
             <p className="text-sm text-neutral-800">
-              Currently only OpenAI&apos; ChatGPT 4o mini supported. Other AI chat engine will be supported in this app.
+              Currently only OpenAI&apos; GPT 4o mini supported. Other AI chat engine will be supported in this app.
             </p>
           </div>
         </div>
--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,50 +1,66 @@
 "use client"
 
 import { useChat } from 'ai/react'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Input } from './ui/input';
 import Image from 'next/image';
 
 export default function Chat() {
     const { messages, input, handleInputChange, handleSubmit } = useChat();
+    const chatContainerRef = useRef(null);
+
+    const Scroll = () => {
+        const { scrollHeight, scrollTop, clientHeight } = chatContainerRef.current
+        const atBottom = scrollHeight - clientHeight <= scrollTop + 1;
+
+        if (!atBottom) {
+            chatContainerRef.current?.scrollTo(0, scrollHeight)
+        }
+    }
+
+    useEffect(() => {
+        Scroll()
+    }, [messages])
 
     return (
-        <div className='flex flex-col w-full max-w-lg py-24 mx-auto min-h-screen'>
-            {messages.map((message) => (
-                <div key={message.id} className={`flex flex-row items-start gap-2 py-1 px-2 ${message.role === 'user' ? 'justify-end' : 'justify-start mb-2'}`}>
-                    {message.role !== 'user' && (
-                        <div className='flex'>
-                            <Image
-                                src='bot-avatar.svg'
-                                alt="Avatar"
-                                width={30}
-                                height={30}
-                            />
+        <>
+            <div className='flex flex-col w-full max-w-lg py-24 mx-auto h-screen overflow-auto' ref={chatContainerRef}>
+                {messages.map((message) => (
+                    <div key={message.id} className={`flex flex-row items-start gap-2 py-1 px-2 ${message.role === 'user' ? 'justify-end' : 'justify-start mb-2'}`}>
+                        {message.role !== 'user' && (
+                            <div className='flex'>
+                                <Image
+                                    src='bot-avatar.svg'
+                                    alt="Avatar"
+                                    width={30}
+                                    height={30}
+                                />
+                            </div>
+                        )}
+                        <div className={`max-w-[90%] flex-col bg-neutral-300 text-slate-900 px-4 py-1 ${message.role === 'user' ? 'rounded-l-xl rounded-br-xl' : 'rounded-r-xl rounded-bl-xl'}`}>
+                            {message.content}
                         </div>
-                    )}
-                    <div className={`max-w-[90%] flex-col bg-neutral-300 text-slate-900 px-4 py-1 ${message.role === 'user' ? 'rounded-l-xl rounded-br-xl' : 'rounded-r-xl rounded-bl-xl'}`}>
-                        {message.content}
+                        {message.role === 'user' && (
+                            <div className='flex-col'>
+                                <Image
+                                    aria-hidden
+                                    src='user-avatar.svg'
+                                    alt="Avatar"
+                                    width={30}
+                                    height={30}
+                                />
+                            </div>
+                        )}
                     </div>
-                    {message.role === 'user' && (
-                        <div className='flex-col'>
-                            <Image
-                                aria-hidden
-                                src='user-avatar.svg'
-                                alt="Avatar"
-                                width={30}
-                                height={30}
-                            />
-                        </div>
-                    )}
-                </div>
-            ))}
+                ))}
+            </div>
             <form onSubmit={handleSubmit} className='fixed bottom-0 w-full p-2 shadow-xl bg-neutral-900'>
                 <Input
-                    className='max-w-lg p-2 mb-4 border border-gray-300 rounded shadow-xl'
+                    className='max-w-lg p-2 mb-4 border border-gray-300 rounded shadow-xl mx-auto'
                     placeholder='ask anything...'
                     value={input}
                     onChange={handleInputChange} />
             </form>
-        </div>
+        </>
     )
 }
